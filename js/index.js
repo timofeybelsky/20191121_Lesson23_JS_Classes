fc618ab7e5948fe7a27333e8e79a846947309bb7
Timofey Belsky <t1m0f3y@gmail.com>
2019-11-21 18:57:42 +0200

    Fibonachi - Iteration function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
 'use strict';
 
-function counterCreator() {
+/*
 
-    let count = 0;
+1 1 2 3 5 8 13 21 34 55 89
 
-    return {
+*/
 
-        increment() {
-            return ++count;
-        },
 
-        decrement() {
-            return --count;
-        },
+function fib(n) {
+
+    if(n < 1 && Number.isInteger(n) && Number.isFinite(n) ) {
+        return;
     }
+
+    if(n <= 2) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
 }
 
-const counter1 = counterCreator();
+function fibI(n) {
 
-const counter2 = counterCreator();
+    if(n < 1 && Number.isInteger(n) && Number.isFinite(n) ) {
+        return;
+    }
+
+    if( n < 3 ) {
+        return 1;
+    }
+
+    let fib1 = 1;
+    let fib2 = 1;
+    let sum = fib1 + fib2;
+
+    for(let i = 3; i < n; i++) {
+
+        fib1 = fib2;
+        fib2 = sum;
+        sum = fib1 + fib2;
+    }
+
+    return sum;
+}
