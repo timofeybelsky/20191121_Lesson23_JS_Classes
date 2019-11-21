ab730334962146e4c60fd25aa9e1ffaa8524e854
Timofey Belsky <t1m0f3y@gmail.com>
2019-11-21 18:26:36 +0200

    Closure - Замыкания

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 'use strict';
 
+function counterCreator() {
+
+    let count = 0;
+
+    return {
+
+        increment() {
+            return ++count;
+        },
+
+        decrement() {
+            return --count;
+        },
+    }
+}
+
+const counter1 = counterCreator();
+
+const counter2 = counterCreator();
